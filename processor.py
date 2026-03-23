02e00255e084af761f892e7ee64bc2a29abde3f4
USHAKOVVVVV <dvadcatpyatyi@gmail.com>
2026-03-23 23:04:03 +0300

    Add fast fix for tax calculation

--- processor.py
+++ processor.py
@@ -1,4 +1,7 @@
 def process_data(data):
-    # Простая и понятная логика
-    result = [x * 1.1 for x in data if x > 0]
+    # Добавили налог 13% хардкодом
+    result = []
+    for x in data:
+        if x > 0:
+            result.append(x * 1.1 * 1.13) # Техдолг: магические числа
     return result
